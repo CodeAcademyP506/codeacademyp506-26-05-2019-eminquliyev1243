8e9b6df5db6dba21666164175c564881c9f949ed
eminquliyev1243 <44087699+eminquliyev1243@users.noreply.github.com>
2019-05-31 16:17:24 +0400

    Exercise is ready to use

---
+++ emin.js
@@ -0,0 +1,76 @@
+// var max=0;
+// var arr = [5,7,4,16,6];
+// for(var i=0;i < arr.length;i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+
+
+// ///////////////////////////>
+
+// var arr=[5,7,4,8,6];
+// var min=arr[0];
+// for(var i = 0;i<arr.length;i++){
+//     if(arr[i] < min){
+//         min = arr[i];
+//     }
+// }
+// console.log(min);
+
+/////////////////////////>
+
+
+// var result=1;
+// for(count = 1;count <= 5;count++){
+// result=result * count;
+// }
+// console.log(result);
+
+///////////////> 
+
+// var arr=[1,2,5,7,3,9,11,12,9,13];
+// var max = arr[0];
+// var min = arr[0];
+// for(var i = 0;i < arr.length;i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+// }
+// for(i=0;i < arr.length;i++){
+//     if(arr[i] > min && arr[i] < max){
+//         min = arr[i];
+//     }
+// }
+// console.log(max * min);
+
+/////////////////////>>>
+// var arr=[1,2,5,8,3,9,11,12];
+//  var even = [];
+// var add=[];
+// for(var i = 0;i < arr.length;i++){
+//     if(arr[i] % 2 == 0){
+//         even.push (arr[i]);
+//     }else{
+//         add.push(arr[i]);
+//     }
+// }
+// console.log(even,add);
+
+
+////////////>
+
+// var num = 12343;
+// var count = 0;
+// if (num >= 0) {
+//     while (num > 0) {
+//         qaliq = num % 10;
+//         num = (num - qaliq) / 10;
+//         count++;
+//     }
+//     console.log(count);
+// }else{
+//     console.log("sizin isiniz bitdi");
+// }
+ /////////////>
